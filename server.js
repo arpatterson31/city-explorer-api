64060d3f243bd0070d7c8039466037fda153c43f
AUDREY PATTERSON <audrey.patterson31@gmail.com>
2021-03-23 14:54:39 -0700

    added notes for myself in the server file for my TODOs and started my forecast constructor, added function to handle error from API call

--- server.js
+++ server.js
@@ -1,31 +1,39 @@
 'use strict';
 
+// require statements
 const express = require('express');
-
 require('dotenv').config();
-
+const cors = require('cors');
 const weatherData = require('./data/weather.json');
 
-const cors = require('cors');
 
 const app = express();
+const PORT = process.env.PORT || 3000;
 
 app.use(cors());
-
-const PORT = process.env.PORT || 3000;
 
 app.get('/', function (request, response) {
   response.send('Hey girl, hey!');
 });
 
-// app.get('/weather', (request, response) => {
-//   response.send('bananas are great')
-// })
+// api endpoint that process at a get request for lat and lon
+app.get('/weather', (request, response) => {
+  response.send('add something here')
+});
 
-// // examples in class today
-// app.get('/shopping', (request, response)=> {
-//   response.send(shoppingList)
-// })
+// constructor function for a Forecast - date and description
+class Forecast {
+  constructor(date, description) {
+    this.date = date;
+    this.description = description;
+  }
+}
+
+
+// function to handle error from any API call
+app.get('*', (request, response) => {
+  response.status(500).send('Internal Server Error');
+});
 
 // turns on the server
 app.listen(PORT, () => console.log(`listening on ${PORT}`));
