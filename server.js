dc1d2cfedd778e079d4bfc493b1c32cdf93ba2ab
AUDREY PATTERSON <audrey.patterson31@gmail.com>
2021-03-25 15:10:47 -0700

    movie route set up and pushing data! yaygit add .

--- server.js
+++ server.js
@@ -49,16 +49,43 @@ function Forecast(obj) {
 }
 
 // api movie endpoint
-// app.get('/movie', getMovie);
+app.get('/movies', getMovies);
 
-// function getMovie (request, response){
+function getMovies (request, response){
+  console.log('in movies:', request.query);
+  const locationSearch = request.query.city;
+  const url = 'https://api.themoviedb.org/3/search/movie';
+  const query = {
+    query: locationSearch,
+    api_key: process.env.MOVIE_API_KEY
+  };
 
-//   const url = 'https://api.themoviedb.org/3/search/movie';
-//   const query = {
-//     city,
-//     key: process.env.MOVIE_API_KEY
-//   };
+  superagent
+    .get(url)
+    .query(query)
+    .then(superagentResults => {
+      const movieResults = superagentResults.body.results;
+      // console.log(superagentResults.body);
+      const movieArray = movieResults.map(movie => new Movie (movie));
+      console.log(movieArray);
+      response.status(200).send(movieArray);
+    })
+    .catch(error => {
+      console.log(error);
+      response.status(404).send('page not found');
+    });
+}
 
+// constructor function for a movie - title, overview, image_url, average_votes, total_votes, popularity, realeased_on
+function Movie(obj) {
+  this.title = obj.title;
+  this.overview = obj.overview;
+  this.imageUrl = obj.poster_path;
+  this.avgVotes = obj.vote_average;
+  this.totalVotes = obj.vote_count;
+  this.popularity = obj.popularity;
+  this.releasedOn = obj.release_date;
+}
 // // function to handle error from any API call
 app.get('*', (request, response) => {
   response.status(500).send('Internal Server Error');
