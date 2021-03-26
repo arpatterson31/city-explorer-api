75dfb7ff8f866dae19132077f9e541ddca4f932d
AUDREY PATTERSON <audrey.patterson31@gmail.com>
2021-03-25 17:09:03 -0700

    completed lab 08 with the movies pushing to the front end

--- server.js
+++ server.js
@@ -57,7 +57,8 @@ function getMovies (request, response){
   const url = 'https://api.themoviedb.org/3/search/movie';
   const query = {
     query: locationSearch,
-    api_key: process.env.MOVIE_API_KEY
+    api_key: process.env.MOVIE_API_KEY,
+    include_adult: false
   };
 
   superagent
@@ -79,8 +80,9 @@ function getMovies (request, response){
 // constructor function for a movie - title, overview, image_url, average_votes, total_votes, popularity, realeased_on
 function Movie(obj) {
   this.title = obj.title;
+  this.movieID = obj.id;
   this.overview = obj.overview;
-  this.imageUrl = obj.poster_path;
+  this.imageUrl = `https://image.tmdb.org/t/p/w300/${obj.poster_path}`;
   this.avgVotes = obj.vote_average;
   this.totalVotes = obj.vote_count;
   this.popularity = obj.popularity;
