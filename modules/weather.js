83ff4c69b9a48a8cb591e12ef9abeb981822c76b
AUDREY PATTERSON <audrey.patterson31@gmail.com>
2021-03-26 21:30:06 -0700

    start of lab 10 -- added modules folder, copied starter code from the class repo on weather.js and fingers crossed fixed all the errors

---
+++ modules/weather.js
@@ -0,0 +1,50 @@
+'use strict';
+
+const superagent = require('superagent');
+let cache = require('./cache.js');
+
+function getWeather(latitude, longitude) {
+  const key = 'weather-' + latitude + longitude;
+  const url = 'http://api.weatherbit.io/v2.0/forecast/daily';
+  const queryParams = {
+    key: process.env.WEATHER_API_KEY,
+    lang: 'en',
+    lat: latitude,
+    lon: longitude,
+    days: 7,
+  };
+
+  if (cache[key] && (Date.now() - cache[key].timestamp < 50000)) {
+    console.log('Cache hit');
+  } else {
+    console.log('Cache miss');
+    cache[key] = {};
+    cache[key].timestamp = Date.now();
+    cache[key].data = superagent
+      .get(url)
+      .query(queryParams)
+      .then(response => parseWeather(response.body));
+  }
+
+  return cache[key].data;
+}
+
+function parseWeather(weatherData) {
+  try {
+    const weatherSummaries = weatherData.data.map(day => {
+      return new Weather(day);
+    });
+    return Promise.resolve(weatherSummaries);
+  } catch (error) {
+    return Promise.reject(error);
+  }
+}
+
+class Weather {
+  constructor(day) {
+    this.forecast = day.weather.description;
+    this.time = day.datetime;
+  }
+}
+
+module.exports = getWeather;
--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 
-const getWeather = require('./weather');
+const getWeather = require('./modules/weather');
 const getMovies = require('./movie');
 
 // routes
